5fe0570f0b4dd1eefe2ccbeb40c36155243dae6a
Shubham gupta Ggps <24877361+gptshubham595@users.noreply.github.com>
2023-12-21 17:54:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const NodeMediaServer = require('node-media-server');
 
 const config = {
   rtmp: {
-    port: 1935,
+    port: 8080,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
-    ping_timeout: 60
+    ping_timeout: 60,
   },
   http: {
-    port: 8000,
-    allow_origin: '*'
-  }
+    port: 80,
+    allow_origin: '*',
+  },
 };
 
-var nms = new NodeMediaServer(config)
+const nms = new NodeMediaServer(config);
+
+nms.on('prePublish', (id, streamPath, args) => {
+  console.log('[NodeEvent on prePublish]', `id=${id} streamPath=${streamPath} args=${JSON.stringify(args)}`);
+  // 在这里你可以执行预发布操作
+});
+
+nms.on('donePublish', (id, streamPath, args) => {
+  console.log('[NodeEvent on donePublish]', `id=${id} streamPath=${streamPath} args=${JSON.stringify(args)}`);
+  // 在这里你可以执行完成发布操作
+});
+
 nms.run();
